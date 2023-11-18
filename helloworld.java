544adc6c0c964e4f2a1766c60df48e82fe26d22f
Nour568 <122759367+Nour568@users.noreply.github.com>
2023-11-18 19:07:09 +0100

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+// Déclaration de la classe HelloWorld
+public class HelloWorld {
+    // Méthode principale
+    public static void main(String[] args) {
+        // Instructions à exécuter lorsque le programme démarre
+        System.out.println("Hello, World!"); // Affiche "Hello, World!" dans la console
+    }
+}
